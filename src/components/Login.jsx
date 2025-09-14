1263022de4894511ea4a44a35dd89b77086d0394
jonatasrf <jonatasrf@gmail.com>
2025-09-14 14:37:10 -0300

    fix: Hardcode a URL de redirecionamento para o GitHub Pages

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -123,7 +123,7 @@ export default function Login() {
             const { error } = await supabase.auth.signInWithOAuth({
                 provider: 'google',
                 options: {
-                    redirectTo: window.location.origin + window.location.pathname
+                    redirectTo: 'https://jonatasrf.github.io/FalanteApp/#'
                 }
             });
             if (error) throw error;
